f5142f21d1affcc4eb0426c547fc602b1917c23d
Theophile Baggio <77689297+badgiojuni@users.noreply.github.com>
2025-01-22 17:34:15 +0000

    chart editing

--- mlops/unit_1_data_preparation/transformers/hyperparameter_tuning/sklearn.py
+++ mlops/unit_1_data_preparation/transformers/hyperparameter_tuning/sklearn.py
@@ -3,6 +3,7 @@ from typing import Dict, Tuple, Union, Callable
 from pandas import Series
 from scipy.sparse._csr import csr_matrix
 from sklearn.base import BaseEstimator
+from mlops.utils.logging import track_experiment
 
 from mlops.utils.models.sklearn import load_class,tune_hyperparameters
 
@@ -33,6 +34,7 @@ def hyperparameter_tuning(
         y_train=y_train,
         X_val=X_val,
         y_val=y_val,
+        callback=lambda **opts: track_experiment(**opts, **kwargs),
         max_evaluations=kwargs.get('max_evaluations'),
         random_state = kwargs.get('random_state'),
     )
